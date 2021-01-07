ba3a67810814fa2c6f144ac88c15287ce222218e
prd-hoang-ta <hoang.ta@paradox.ai>
2021-01-07 19:21:45 +0700

    [IDM-1] Add system out

--- src/main/java/com/dut/bookstore/controller/BookController.java
+++ src/main/java/com/dut/bookstore/controller/BookController.java
@@ -39,6 +39,7 @@ public class BookController {
 
     @PutMapping
     public ResponseEntity updateBook(@RequestBody Book book) {
+        System.out.println("Chao Ban");
         return new ResponseEntity<>(bookService.updateBook(book), HttpStatus.OK);
     }
 
